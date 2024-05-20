8e1a871339c9cbc0b88ed1742e970619ce41a9e8
Bipin Adhikari <adhikaribipin1032@gmail.com>
2024-05-20 18:01:12 +0545

    Complete hero section

---
+++ bankist-app/src/components/Mid.jsx
@@ -0,0 +1,23 @@
+import "../css/./mid.css";
+
+const Mid = () => {
+  return (
+    <>
+      <div className="middle-section">
+        <div className="middle-header">
+          <p className="text-wrapper features">features</p>
+          <h2 className="section-header">
+            Everything you need in a modern bank <br /> and more.
+          </h2>
+        </div>
+        <div className="container middle-section-wrapper">
+          <div className="middle-image">
+            <img src="../assets/images/digital.jpg" alt="digital image" />
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Mid;
--- bankist-app/src/pages/Home.jsx
+++ bankist-app/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
+import Mid from "../components/Mid";
 
 const Home = () => {
   return (
     <>
       <Navigation />
       <Hero />
+      <Mid />
     </>
   );
 };
